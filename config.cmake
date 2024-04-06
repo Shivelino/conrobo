057b9d446d1c6f70b97f597a0f48b932ce2c3cd5
Shivelino <shivelino@qq.com>
2024-04-06 22:15:54 +0800

    chore: fix wrong version

--- config.cmake
+++ config.cmake
@@ -2,6 +2,6 @@
 # Project Config
 # ---------------------------------------------------------------------------------------
 set(MY_PROJ_NAME "conrobo")
-set(MY_PROJ_VERSION_MAJOR 1)
+set(MY_PROJ_VERSION_MAJOR 0)
 set(MY_PROJ_VERSION_MINOR 0)
-set(MY_PROJ_VERSION_PATCH 0)
+set(MY_PROJ_VERSION_PATCH 1)
