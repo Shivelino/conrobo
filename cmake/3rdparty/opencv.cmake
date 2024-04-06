08744377124f8914ed0b4da39c3a567adc890804
Shivelino <shivelino@qq.com>
2024-04-07 01:33:30 +0800

    chore: format

--- cmake/3rdparty/opencv.cmake
+++ cmake/3rdparty/opencv.cmake
@@ -9,15 +9,21 @@ function(find_opencv)
         message(FATAL_ERROR "Third party NOT found in system.")
     endif()
 
-    set(Conrobo_OpenCV_INCLUDE_DIRS ${OpenCV_INCLUDE_DIRS} PARENT_SCOPE)
-    set(Conrobo_OpenCV_LIBRARIES ${OpenCV_LIBS} PARENT_SCOPE)
+    set(Conrobo_OpenCV_INCLUDE_DIRS
+        ${OpenCV_INCLUDE_DIRS}
+        PARENT_SCOPE)
+    set(Conrobo_OpenCV_LIBRARIES
+        ${OpenCV_LIBS}
+        PARENT_SCOPE)
     set(_Conrobo_OpenCV_BIN_DIR)
 
     if(WIN32)
         set(_Conrobo_OpenCV_BIN_DIR "${OpenCV_INSTALL_PATH}/x64/vc16/bin")
     endif()
 
-    set(Conrobo_OpenCV_BIN_DIR ${_Conrobo_OpenCV_BIN_DIR} PARENT_SCOPE)
+    set(Conrobo_OpenCV_BIN_DIR
+        ${_Conrobo_OpenCV_BIN_DIR}
+        PARENT_SCOPE)
 
     message(STATUS "=================================================================")
 endfunction(find_opencv)
@@ -37,4 +43,7 @@ function(target_link_opencv _target_name _dll_dest_dir)
             POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different ${dll_file} ${_dll_dest_dir})
     endforeach(dll_file ${_moving_dlls})
-endfunction(target_link_opencv _target_name _dll_dest_dir)
+endfunction(
+    target_link_opencv
+    _target_name
+    _dll_dest_dir)
